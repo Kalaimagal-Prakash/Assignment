40cc7320a4a714efe44ae6c4527c1ff4d238c483
Kalaimagal <Kalaimagal.Prakash@trumpf.com>
2024-08-09 17:25:41 +0530

    Revert "Assignment2 : Submission"
    This reverts commit ce243a5bd2470569ebf1d01dbfd82edb75ed826f.

--- Training24/intlist.c
+++ Training24/intlist.c
@@ -1,6 +1,21 @@
 // ------------------------------------------------------------------------------------------------
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Implement a linked list data structure to handle integer elements. The implementation will consist of three files:
+// intlist.h - the header file
+// intlist.c - the code.
+// testintlist.c - the program that tests the implementation.
+// -------------------------------------------------------------------
+// The following functions have to be implemented:
+// a)	Create – this will create a new linked list.
+// b)	Delete – this will delete the list and all the linked elements.
+// c)	Add – this will add an element to the end of the list.
+// d)	Insert – this will insert an element at a particular index(zero based).For example, if the linked list has 4 elements.Calling Insert with an index of 2 will insert the new element between that at index 1 and that at index 2.
+// e)	RemoveAt – this will remove the element at a particular index(zero based).
+// f)	Remove – removes the first occurrence of a specific element.
+// g)	Count – number of elements in the list.
+// h)	Get – gets the element at a particular index.
 // ------------------------------------------------------------------------------------------------
 // intlist.c
 // Program on main branch.
