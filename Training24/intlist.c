671eb981b8ae6e0604a63f97cb8f9641d067b8ca
Kalaimagal <Kalaimagal.Prakash@trumpf.com>
2024-08-21 16:30:02 +0530

    Assignment2: Corrected

--- Training24/intlist.c
+++ Training24/intlist.c
@@ -10,42 +10,37 @@
 #include <malloc.h>
 #include "intlist.h"
 
-//Create an empty linked list
 void Create (LinkList* list) {
    list->head = NULL;
    list->size = 0;
 }
 
-//Delete the linked list and all elements
 void DeleteList (LinkList* list) {
-   struct Node* current = list->head;
+   struct Node* current = list->head;  // Start from the head of the list
    struct Node* nextNode;
-   while (current != NULL) {
+   while (current != NULL) {          //Traverse the list and free each node
       nextNode = current->link;
-      free (current);
+      free (current);                
       current = nextNode;
    }
-   list->head = NULL;
+   list->head = NULL;                
    list->size = 0;
 }
 
-//Add an element to the end of the list.
 void Add (LinkList* list, int value) {
    if (list == NULL)
       return;
-
    struct Node* newNode = (struct Node*)malloc (sizeof (struct Node));
    if (newNode == NULL) 
       return ERROR_MEMORY_ALLOCATION;
-
    newNode->data = value;
    newNode->link = NULL;
    if (list->head == NULL) {
-      list->head = newNode;
+      list->head = newNode;                // If the list is empty the new node becomes the head
    }
    else {
       struct Node* lastNode = list->head;
-      while (lastNode->link != NULL) {
+      while (lastNode->link != NULL) {      // Traverse to the end of the list
          lastNode = lastNode->link;
       }
       lastNode->link = newNode;
@@ -53,11 +48,9 @@ void Add (LinkList* list, int value) {
    list->size++;
 }
 
-//Insert an element at a specific index
 void Insert (LinkList* list, int index, int value) {
-   if (index < 0 || index > list->size) {
+   if (index < 0 || index > list->size) 
       return ERROR_OUT_OF_BOUNDS;
-   }
    struct Node* newNode = (struct Node*)malloc (sizeof (struct Node));
    struct Node* currentNode = list->head;
    newNode->data = value;
@@ -66,7 +59,7 @@ void Insert (LinkList* list, int index, int value) {
       list->head = newNode;
    }
    else {
-      for (int i = 0; i < index - 1; i++) {
+      for (int i = 0; i < index - 1; i++) {     // Traverse to the node before the insertion
          currentNode = currentNode->link;
       }
       newNode->link = currentNode->link;
@@ -75,11 +68,9 @@ void Insert (LinkList* list, int index, int value) {
    list->size++;
 }
 
-//Remove an element at a specific -index
 void RemoveAt (LinkList* list, int index) {
-   if (index < 0 || index > list->size) {
+   if (index < 0 || index > list->size) 
       return ERROR_OUT_OF_BOUNDS;
-   }
    struct Node* currentNode = list->head;
    struct Node* previousNode = NULL;
    if (index == 0) {
@@ -87,7 +78,7 @@ void RemoveAt (LinkList* list, int index) {
       free (currentNode);
    }
    else {
-      for (int i = 0; i < index; i++) {
+      for (int i = 0; i < index; i++) {         // Traverse to the node before the removal
          previousNode = currentNode;
          currentNode = currentNode->link;
       }
@@ -97,42 +88,34 @@ void RemoveAt (LinkList* list, int index) {
    list->size--;
 }
 
-// Remove the first occurrence of a node with specific value
 void Remove (LinkList* list, int value) {
    struct Node* currentNode = list->head;
    struct Node* previousNode = NULL;
-   // Traverse the list to find the node with the specified value
-   while (currentNode != NULL && currentNode->data != value) {
+   while (currentNode != NULL && currentNode->data != value) {  // Traverse the list to find the node with the specified value
       previousNode = currentNode;
       currentNode = currentNode->link;
    }
    if (currentNode == NULL) 
       return ERROR_ELEMENT_NOT_FOUND;
-   
    if (previousNode == NULL) 
       list->head = currentNode->link;
    else 
       previousNode->link = currentNode->link;
-   
    free (currentNode);
    list->size--;
 }
 
-//Return the number of elements in the list
 int Count (LinkList* list) {
    return list->size;
 }
 
-//Get the element at a specific index
 int Get (const LinkList* list, int index, int* value) {
-   if (index < 0 || index >= list->size) {
+   if (index < 0 || index >= list->size) 
       return ERROR_OUT_OF_BOUNDS;
-   }
    if (list->head == NULL)
       return ERROR_OUT_OF_BOUNDS;
-
    struct Node* currentNode = list->head;
-   for (int i = 0; i < index; i++) {
+   for (int i = 0; i < index; i++) {          // Traverse to the desired index
       currentNode = currentNode->link;
    }
    *value = currentNode->data;
