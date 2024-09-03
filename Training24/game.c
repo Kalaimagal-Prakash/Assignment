b6a8a77726372c659634ada3650bae9d0656a8bc
Kalaimagal <Kalaimagal.Prakash@trumpf.com>
2024-09-03 12:32:28 +0530

    A3: Corrected

--- Training24/game.c
+++ Training24/game.c
@@ -7,12 +7,10 @@
 // Convert the given decimal number to different forms (Decimal, Hexadecimal, Binary) 
 // ------------------------------------------------------------------------------------------------
 
-#include <stdio.h>
-#include <string.h>
 #include <malloc.h>
 #include "game.h"
 
-// Convert a decimal number to its binary representation
+/// <summary>Convert a decimal number to its binary representation.</summary>
 char* DecimalToBinary (int n) {
    char* binaryStr = (char*)malloc (BITS + (BITS / 8) + 1);                 // Allocate memory for binary string with extra space for formatting
    if (binaryStr == NULL) return NULL;
@@ -26,118 +24,15 @@ char* DecimalToBinary (int n) {
    return binaryStr;
 }
 
-// Convert a decimal number to its hexadecimal representation
+/// <summary>Convert a decimal number to its hexadecimal representation.</summary>
 char* DecimalToHex (int h) {
    int index = 0;
    char* hexStr = (char*)malloc (HEX_DIGIT + 3 + 1);                    // Allocate memory for hex string
    if (hexStr == NULL) return NULL;
-   hexStr[index++] = '0';
-   hexStr[index++] = 'x';                                              // Prefix for hexadecimal
    for (int i = 0; i < HEX_DIGIT; i++) {                    
       int digit = (h >> ((HEX_DIGIT - 1 - i) * 4)) & 0xF;              // Extract hex digit
       hexStr[index++] = digit < 10 ? digit + '0' : digit - 10 + 'A';   // Convert digit to character
    }
-   hexStr[index] = '\0';                                              // Null-terminate the string
+   hexStr[index] = '\0';                                               // Null-terminate the string
    return hexStr;
-}
-// Test cases for DecimalToBinary function
-void TestDecToBinAndNegBin () {
-   unsigned int positive_inputs[] = { 5, 45, 10, 214, 255 };
-   const char* expected_bin_result[] = {
-       "00000000 00000000 00000000 00000101",                         // Expected binary results for positive numbers
-       "00000000 00000000 00000000 00101101",
-       "00000000 00000000 00000000 00001010",
-       "00000000 00000000 00000000 11010110",
-       "00000000 00000000 00000000 11111110"
-   };
-   const char* expected_neg_bin_result[] = {                          // Expected binary results for negative numbers (two's complement representation)
-       "11111111 11111111 11111111 11111011",
-       "11111111 11111111 11111111 11010011",
-       "11111111 11111111 11111111 11110110",
-       "11111111 11111111 11111111 00101010",
-       "11111111 11111111 11111111 00000000"  
-   };
-   int noOfTests = sizeof (positive_inputs) / sizeof (positive_inputs[0]);
-   // Test positive cases
-   for (int i = 0; i < noOfTests; i++) {
-      char* result = DecimalToBinary ((int)positive_inputs[i]);
-      if (result != NULL) {
-         if (strcmp (result, expected_bin_result[i]) == 0) printf ("Positive Test Case %d: Input --> %u PASS\n", i + 1, positive_inputs[i]);
-         else {
-            printf ("Positive Test Case %d:  FAIL\n", i + 1);
-            printf ("EXPECTED: %s\n", result);
-            printf ("GOT: %s\n", expected_bin_result[i]);
-         }
-         free (result);
-      }
-      else {
-         printf ("Positive Test Case %d: FAIL\n", i + 1);
-         printf ("EXPECTED: %s\n", expected_bin_result[i]);
-         printf ("GOT: NULL (Memory allocation failed)\n");
-      }
-      printf ("\n");
-   }
-   // Test negative cases
-   for (int i = 0; i < noOfTests; i++) {
-      char* result = DecimalToBinary (-(int)positive_inputs[i]);
-      if (result != NULL) {
-         if (strcmp (result, expected_neg_bin_result[i]) == 0) printf ("Negative Test Case %d: Input --> -%u PASS\n", i + 1, positive_inputs[i]);
-         else {
-            printf ("Negative Test Case %d: FAIL\n", i + 1);
-            printf ("EXPECTED: %s\n", result);
-            printf ("GOT: %s\n", expected_neg_bin_result[i]);
-         }
-         free (result);
-      }
-      else {
-         printf ("Negative Test Case %d: FAIL\n", i + 1);
-         printf ("EXPECTED: %s\n", expected_neg_bin_result[i]);
-         printf ("GOT: NULL (Memory allocation failed)\n");
-      }
-      printf ("\n");
-   }
-}
-
-// Test cases for DecimalToHex function
-void TestDecToHexAndNegHex () {
-   unsigned int inputs[] = { 6, 9, 255, 78, 34 };
-   const char* expected_result_pos[] = {
-       "0x00000006",                                 // Expected hexadecimal results for positive numbers
-       "0x00000009",
-       "0x000000FF",
-       "0x0000004E",
-       "0x00000020"
-   };
-   const char* expected_result_neg[] = {
-       "0xFFFFFFFA",                                // Expected hexadecimal results for negative numbers
-       "0xFFFFFFF7",
-       "0xFFFFFF01",
-       "0xFFFFFFB2",
-       "0xFFFFFFD0"
-   };
-   int noOfTests = sizeof (inputs) / sizeof (inputs[0]);
-   // Test positive values
-   for (int i = 0; i < noOfTests; i++) {
-      char* result = DecimalToHex (inputs[i]);
-      if (strcmp (result, expected_result_pos[i]) == 0) printf ("Test Case %d (Positive): Input --> %u Pass\n", i + 1, inputs[i]);
-      else {
-         printf ("Test Case %d (Positive): FAIL\n", i + 1);
-         printf ("EXPECTED: %s\n", result);
-         printf ("GOT: %s\n", expected_result_pos[i]);
-      }
-      free (result);
-      printf ("\n");
-   }
-   // Test negative values
-   for (int i = 0; i < noOfTests; i++) {
-      char* result = DecimalToHex (-((int)inputs[i]));
-      if (strcmp (result, expected_result_neg[i]) == 0) printf ("Test Case %d (Negative): Input ---> -%u Pass\n", i + 1, inputs[i]);
-      else {
-         printf ("Test Case %d (Negative): FAIL\n", i + 1);
-         printf ("EXPECTED: %s\n", result);
-         printf ("GOT: %s\n", expected_result_neg[i]);
-      }
-      free (result);
-      printf ("\n");
-   }
 }
--- Training24/test.c
+++ Training24/test.c
@@ -8,12 +8,19 @@
 // ------------------------------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include "game.h"
 
-// Function to clear the console screen
-void clearScreen () {
+// ANSI escape codes for colors
+#define COLOR_RESET   "\033[0m"
+#define COLOR_GREEN   "\033[32m"
+#define COLOR_RED     "\033[31m"
+#define COLOR_YELLOW  "\033[33m"
+
+/// <summary>Function to clear the console screen.</summary>
+void ClearScreen () {
 #ifdef _WIN32
    system ("cls");              // Windows-specific command to clear the screen
 #else
@@ -21,13 +28,97 @@ void clearScreen () {
 #endif
 }
 
+/// <summary>Function to print test results for binary conversion.</summary>
+void FormatBinary (int numTests, int inputs[], const char* expected_results[], const char* actual_results[], int pass[]) {
+   // Adjust column widths for alignment
+   printf (COLOR_YELLOW "-------------------------------------------- Test Results for Binary ------------------------------------------\n" COLOR_RESET);
+   printf ("| Test Case | Input       | Expected                            | Actual                               |Result|\n");
+   printf ("|-----------|-------------|-------------------------------------|--------------------------------------|------|\n");
+   for (int i = 0; i < numTests; i++) {
+      // Adjust column widths to match header alignment
+      printf ("| %-9d | %-11d | %-35s | %-36s | %-6s |\n", i + 1, inputs[i], expected_results[i], actual_results[i] ? actual_results[i] : "NULL",
+         pass[i] ? COLOR_GREEN "PASS" COLOR_RESET : COLOR_RED "FAIL" COLOR_RESET);
+   }
+   printf ("\n");
+}
+
+/// <summary>Function to print test results for Hexadecimal conversion.</summary>
+void FormatHexadecimal (int numTests, int inputs[], const char* expected_results[], const char* actual_results[], int pass[]) {
+   // Adjust column widths for alignment
+   printf (COLOR_YELLOW "------------------- Test Results Hexadecimal -------------------\n" COLOR_RESET);
+   printf ("| Test Case | Input       | Expected     | Actual       |Result|\n");
+   printf ("|-----------|-------------|--------------|--------------|------|\n");
+   for (int i = 0; i < numTests; i++) {
+      // Adjust column widths to match header alignment
+      printf ("| %-9d | %-11d | %-12s | %-12s | %-6s |\n", i + 1, inputs[i], expected_results[i], actual_results[i] ? actual_results[i] : "NULL",
+         pass[i] ? COLOR_GREEN "PASS" COLOR_RESET : COLOR_RED "FAIL" COLOR_RESET);
+   }
+   printf ("\n");
+}
+
+/// <summary>Test cases for DecimalToBinary function.</summary>
+void TestDecToBinAndNegBin () {
+   // Define input test values and expected binary results
+   int inputs[] = { 5, 45, 214, 2147483647, 255, -5, -255, -2147483647 };
+   const char* expected_bin_result[] = {
+       "00000000 00000000 00000000 00000101",  // 5
+       "00000000 00000000 00000000 00101101",  // 45
+       "00000000 00000000 00000000 11010110",  // 214
+       "01111111 11111111 11111111 11111111",  // 2147483647
+       "00000000 00000000 00000000 11111111",  // 255
+       "11111111 11111111 11111111 11111011",  // -5
+       "11111111 11111111 11111111 00000001",  // -255
+       "10000000 00000000 00000000 00000001"   // -2147483647
+   };
+   char* actual_bin_result[sizeof (inputs) / sizeof (inputs[0])];                                    // Allocate arrays to store actual results and pass/fail status
+   int pass[sizeof (inputs) / sizeof (inputs[0])];
+   for (int i = 0; i < sizeof (inputs) / sizeof (inputs[0]); i++) {
+      char* result = DecimalToBinary (inputs[i]);
+      if (result && strcmp (result, expected_bin_result[i]) == 0) pass[i] = 1;                      // Pass
+      else pass[i] = 0;                                                                             // Fail
+      actual_bin_result[i] = result;
+   }
+   // Call the function to format and print the results
+   FormatBinary (sizeof (inputs) / sizeof (inputs[0]), inputs, expected_bin_result, actual_bin_result, pass);
+   for (int i = 0; i < sizeof (inputs) / sizeof (inputs[0]); i++) free (actual_bin_result[i]);      // Free allocated memory
+}
+
+/// <summary>Test cases for DecimalToHex function.</summary>
+void TestDecToHexAndNegHex () {
+   // Define input test values and expected hexadecimal results
+   int inputs[] = { 1, 255, 4095, 65536, 2147483647, -2147483647, -255, -1 };
+   const char* expected_hex_result[] = {
+       "00000001",  // 1
+       "000000FF",  // 255
+       "00000FFF",  // 4095
+       "00010000",  // 65536
+       "7FFFFFFF",  // 2147483647
+       "80000001",  // -2147483647
+       "FFFFFF01",  // -255
+       "FFFFFFFF"   // -1
+   };
+   char* actual_hex_result[sizeof (inputs) / sizeof (inputs[0])];                                 // Allocate arrays to store actual results and pass/fail status
+   int pass[sizeof (inputs) / sizeof (inputs[0])];
+   for (int i = 0; i < sizeof (inputs) / sizeof (inputs[0]); i++) {
+      char* result = DecimalToHex (inputs[i]);
+      if (result && strcmp (result, expected_hex_result[i]) == 0) pass[i] = 1;                   // Pass
+      else pass[i] = 0;                                                                          // Fail
+      actual_hex_result[i] = result;
+   }
+   // Call the function to format and print the results
+   FormatHexadecimal (sizeof (inputs) / sizeof (inputs[0]), inputs, expected_hex_result, actual_hex_result, pass);
+   for (int i = 0; i < sizeof (inputs) / sizeof (inputs[0]); i++) free (actual_hex_result[i]);   // Free allocated memory
+}
+
 int main () {
-   int num;                                          // Variable to store the user input
-   char choice,buffer[100];
+   // Run test cases
+   TestDecToBinAndNegBin ();                                              // Call test function for decimal to binary conversion
+   TestDecToHexAndNegHex ();                                              // Call test function for decimal to hexadecimal conversion
+   char choice;
    do {
-      clearScreen ();
-      int validInput = 0;                            // Check the input is Valid
-      while (!validInput) {                          // Prompt the user for input
+      int num, validInput = 0;                                           // Variable to store the user input
+      char buffer[100];
+      while (!validInput) {                                              // Prompt the user for input
          printf ("Enter a decimal number: ");
          if (fgets (buffer, sizeof (buffer), stdin)) {
             char* endptr;
@@ -42,29 +133,23 @@ int main () {
             if (choice != 'y' && choice != 'Y') printf ("Exiting the program.\n");
          }
       }
-      char* binaryStr = DecimalToBinary (num);                                  // Convert the decimal number to binary and print the result
+      char* binaryStr = DecimalToBinary (num);                          // Convert the decimal number to binary
       if (binaryStr != NULL) {
          printf ("Binary: %s\n", binaryStr);
          free (binaryStr);
       }
-      else printf ("Memory allocation Failed.\n");
-      char* hexStr = DecimalToHex (num);                                      // Convert the decimal number to hexadecimal and print the result
+      else printf ("Memory allocation failed.\n");
+      char* hexStr = DecimalToHex (num);                               // Convert the decimal number to hexadecimal
       if (hexStr != NULL) {
          printf ("Hexadecimal: %s\n", hexStr);
          free (hexStr);
       }
-      printf ("Decimal: %d\n", num);                                         // Print the decimal number
-      printf ("\nDo you want to enter another number? (y/n): ");             // Ask the user if they want to enter another number
+      else printf ("Memory allocation failed.\n");
+      printf ("Decimal: %d\n", num);                                  // Print the decimal number
+      printf ("\nDo you want to enter another number? (y/n): ");      // Ask if user wants to continue
       choice = getchar ();
       while (getchar () != '\n');
-   } while (choice == 'y' || choice == 'Y');                                // Repeat the process if the user chooses 'y' or 'Y'
-   // Test conversion functions
-   printf ("----------- Decimal To Binary ---------- \n");
-   TestDecToBinAndNegBin ();                                               // Call test function for decimal to binary conversion
-   printf ("----------- Decimal To Hexadecimal ----------- \n");
-   TestDecToHexAndNegHex ();                                               // Call test function for decimal to hexadecimal conversion
+   } while (choice == 'y' || choice == 'Y');                          // Repeat if 'y' or 'Y'
+   printf ("Exiting the program.\n");
    return 0;
 }
-
-
-
