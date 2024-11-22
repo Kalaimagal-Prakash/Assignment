1f94e48bbbeb742e7f55acf6203b6163064390e0
Kalaimagal <Kalaimagal.Prakash@trumpf.com>
2024-11-22 13:00:59 +0530

    Test2: Game Board

--- Training24/ClassicLibrary.c
+++ Training24/ClassicLibrary.c
@@ -5,8 +5,26 @@
 // Classiclibrary.c
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-char input[] = "Hello World!"; // Global character array
+#include <stdio.h>
 
-char* GetString () {
-   return input; // Return the global greeting
+void board () {
+   for (int i = 0; i < 3; i++) {
+      wprintf (L"\u250F\u2501\u2501\u2533\u2501\u2501\u2533\u2501\u2501\u2513\n");
+      wprintf (L"\u2503  \u2503  \u2503  \u2503\n");
+      wprintf (L"\u2523\u2501\u2501\u254B\u2501\u2501\u254B\u2501\u2501\u252B\n");
+      wprintf (L"\u2503  \u2503  \u2503  \u2503\n");
+      wprintf (L"\u2523\u2501\u2501\u254B\u2501\u2501\u254B\u2501\u2501\u252B\n");
+      wprintf (L"\u2503  \u2503  \u2503  \u2503\n");
+      wprintf (L"\u2517\u2501\u2501\u253B\u2501\u2501\u253B\u2501\u2501\u251B\n");
+      return 0;
+   }
+}
+
+
+int main () {
+   void board ();
+   int num;
+   printf ("Enter the number (1-9):");
+   scanf_s ("%d", &num);
+   return 0;
 }
