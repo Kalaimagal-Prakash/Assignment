688de5405c8f398403a1606da89c05b830f0e92c
Kalaimagal <Kalaimagal.Prakash@trumpf.com>
2024-12-31 12:04:13 +0530

    B1: Initial commit

---
+++ Training24/AssignmentB.c
@@ -0,0 +1,67 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// Kalaimagal V P
+// ------------------------------------------------------------------------------------------------
+// AssignmentB.c
+// Program on B1 branch.
+// ------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+
+static void Fibonacci (int n) {
+   int a = 0, b = 1, next;
+   printf ("%d %d ", a, b);
+   for (int i = 2; i < n; i++) {
+      next = a + b;
+      printf ("%d ", next);
+      a = b;
+      b = next;
+   }
+   printf ("\n");
+}
+
+int IsPrime (int n) {
+   if (n <= 1) return 0;
+   for (int i = 2; i * i <= n; i++) if (n % i == 0) return 0;
+   return 1;
+}
+
+int main () {
+   int n, choice;
+   char buffer[100];
+   char continueChoice;
+   do {
+      printf ("\nChoose an option:\n");
+      printf ("1. Display Fibonacci series up to n terms\n");
+      printf ("2. Check if a number is prime\n");
+      printf ("Enter your choice (1 or 2): ");
+      fgets (buffer, sizeof (buffer), stdin);
+      choice = atoi (buffer);
+      printf ("Enter the value of n: ");
+      fgets (buffer, sizeof (buffer), stdin);
+      n = atoi (buffer);
+      switch (choice) {
+      case 1:
+         printf ("Fibonacci series up to %d terms: ", n);
+         Fibonacci (n);
+         break;
+      case 2:
+         IsPrime (n) ? printf ("%d is a prime number.\n", n) : printf ("%d is not a prime number.\n", n);
+         break;
+      default:
+         printf ("Invalid choice. Please select 1 or 2.\n");
+         break;
+      }
+      printf ("Do you want to continue? (y/n): ");
+      continueChoice = _getche ();
+      continueChoice = tolower (continueChoice);
+      if (continueChoice == 'n') {
+         printf ("\nExit the Program\n");
+         break;
+      }
+   } while (continueChoice == 'y');
+   return 0;
+}
